b3c2fc15a9f67b0c23018f0c8257745117e38160
americamm <amendoza@cicese.edu.mx>
2015-04-23 18:45:57 -0700

    Revisados

--- HandRGBDetection/HandRGBDetection/MainWindow.xaml.cs
+++ HandRGBDetection/HandRGBDetection/MainWindow.xaml.cs
@@ -33,19 +33,19 @@ namespace HandRGBDetection
         private WriteableBitmap ImagenWriteablebitmap;
         private Int32Rect WriteablebitmapRect;
         private int WriteablebitmapStride;
-        private DepthImageStream DepthStream;
-        private byte[] DepthImagenPixeles;
-        private short[] DepthValoresStream;
-        private Image<Gray, Byte> depthFrameKinect;
+        private ColorImageStream ColorStream; 
+        private byte[] ColorImagenPixeles;
+        private Image<Bgra, Byte> colorFrameKinect;
         private CascadeClassifier haar1;
         private bool moverK = false;
         private bool grabar = false;
         private string path;
         private int index;
-        private int numeroGrabaciones = 50;
+        private int numeroGrabaciones = 80;
         private string numeroManos;
         private string tipoIluminacion;
-        private string nameClassifier = "1"; 
+        private string background; 
+        private string nameClassifier = "5"; 
         //:::::::::::::fin variables:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
@@ -81,8 +81,8 @@ namespace HandRGBDetection
                 if (Kinect.Status == KinectStatus.Connected)
                 {
                     Kinect.ColorStream.Enable();
-                    Kinect.DepthStream.Enable();
-                    Kinect.DepthStream.Range = DepthRange.Near;
+                    //Kinect.DepthStream.Enable();
+                    //Kinect.DepthStream.Range = DepthRange.Near;
                     Kinect.Start();
                 }
             }
@@ -95,38 +95,38 @@ namespace HandRGBDetection
 
         private void CompositionTarget_Rendering(object sender, EventArgs e)
         {
-            Image<Gray, Byte> imagenClasificar;
-            Image<Gray, Byte> imageHaar1;
-            Image<Gray, Byte> imageMedianF3;
+            Image<Bgra, Byte> imagenClasificar;
+            Image<Bgra, Byte> imageHaar1;
+            //Image<Bgra, Byte> imageMedianF3;
+            //Image<Bgra, Byte> imageHaar1NoNoise3;
 
-            Image<Gray, Byte> imageHaar1NoNoise3;
 
-
-            imagenClasificar = PollDepth();
-
-            imageMedianF3 = removeNoise(imagenClasificar, 3);
+            imagenClasificar = PollColor();
+            //imageMedianF3 = removeNoise(imagenClasificar, 3);
  
 
             //Call detection an save the image with the result of the classifier.
             imageHaar1 = Detection(haar1, imagenClasificar);
-            imageHaar1NoNoise3 = Detection(haar1, imageMedianF3);
+            //imageHaar1NoNoise3 = Detection(haar1, imageMedianF3);
 
 
             //Display the result of the classifier, so the bytes of the imagen
             //are converted in a wriablebitmap.  
             imageKinect.Source = imagetoWriteablebitmap(imageHaar1);
-            imageMedianFilter.Source = imagetoWriteablebitmap(imageHaar1NoNoise3);
+            //imageMedianFilter.Source = imagetoWriteablebitmap(imageHaar1NoNoise3);
 
             if ((index < (numeroGrabaciones)) && grabar)
             {
                 guardaimagen(imageHaar1, path, index, "Noise");
-                guardaimagen(imageHaar1NoNoise3, path, index, "noNoise");
+                //guardaimagen(imageHaar1NoNoise3, path, index, "noNoise");
 
                 index++;
             }
 
             if (index == numeroGrabaciones)
             {
+                simple.IsEnabled = true;
+                complejo.IsEnabled = true; 
                 iluminacion.IsEnabled = true;
                 noiluminacion.IsEnabled = true;
             }
@@ -134,45 +134,22 @@ namespace HandRGBDetection
         } //fin CompositionTarget_Rendering()  
 
 
-        private Image<Gray, Byte> PollDepth()
+        private Image<Bgra, Byte> PollColor()
         {
             if (this.Kinect != null)
             {
-                this.DepthStream = this.Kinect.DepthStream;
-                this.DepthValoresStream = new short[DepthStream.FramePixelDataLength];
-                this.DepthImagenPixeles = new byte[DepthStream.FramePixelDataLength];
-                this.depthFrameKinect = new Image<Gray, Byte>(DepthStream.FrameWidth, DepthStream.FrameHeight);
+                this.ColorStream = this.Kinect.ColorStream;
+                this.ColorImagenPixeles = new byte[ColorStream.FramePixelDataLength];
+                this.colorFrameKinect = new Image<Bgra, Byte>(ColorStream.FrameWidth, ColorStream.FrameHeight);
 
                 try
                 {
-                    using (DepthImageFrame frame = this.Kinect.DepthStream.OpenNextFrame(100))
+                    using (ColorImageFrame frame = this.Kinect.ColorStream.OpenNextFrame(100))
                     {
                         if (frame != null)
                         {
-                            frame.CopyPixelDataTo(this.DepthValoresStream);
-
-                            int index = 0;
-                            for (int i = 0; i < frame.PixelDataLength; i++)
-                            {
-                                int valorDistancia = DepthValoresStream[i] >> 3;
-
-                                if (valorDistancia == this.Kinect.DepthStream.UnknownDepth)
-                                {
-                                    DepthImagenPixeles[index] = 0;
-                                }
-                                else if (valorDistancia == this.Kinect.DepthStream.TooFarDepth)
-                                {
-                                    DepthImagenPixeles[index] = 0;
-                                }
-                                else
-                                {
-                                    byte byteDistancia = (byte)(255 - (valorDistancia >> 5));
-                                    DepthImagenPixeles[index] = byteDistancia;
-                                }
-                                index++; //= index + 4;
-                            }
-
-                            depthFrameKinect.Bytes = DepthImagenPixeles; //The bytes are converted to a Imagen(Emgu). This to work with the functions of opencv. 
+                            frame.CopyPixelDataTo(this.ColorImagenPixeles);
+                            colorFrameKinect.Bytes = ColorImagenPixeles; //The bytes are converted to a Imagen(Emgu). This to work with the functions of opencv. 
                         }
                     }
                 }
@@ -182,38 +159,42 @@ namespace HandRGBDetection
                 }
             }
 
-            return depthFrameKinect;
-        }//fin PollDepth()
+            return colorFrameKinect;
+        }//fin PollColor()
 
 
         //:::::::::::::Fin de los metodos para manipular los datos del Kinect::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
         //:::::::::::::Methods to do the detection, using haar features and cascade trining, the classifiers already trained::::::::::::
 
-        private Image<Gray, Byte> Detection(CascadeClassifier haar, Image<Gray, Byte> frameDepth)
+        private Image<Bgra, Byte> Detection(CascadeClassifier haar, Image<Bgra, Byte> frame)
         {
-            if (frameDepth != null)
+            Image<Gray, Byte> frameGrayscale;
+
+            frameGrayscale = frame.Convert<Gray, Byte>(); 
+
+            if (frameGrayscale != null)
             {
-                System.Drawing.Rectangle[] hands = haar.DetectMultiScale(frameDepth, 1.4, 0, new System.Drawing.Size(frameDepth.Width / 9, frameDepth.Height / 9), new System.Drawing.Size(frameDepth.Width / 4, frameDepth.Height / 4));
+                System.Drawing.Rectangle[] hands = haar.DetectMultiScale(frameGrayscale, 1.4, 0, new System.Drawing.Size(frameGrayscale.Width / 8, frameGrayscale.Height / 8), new System.Drawing.Size(frameGrayscale.Width /3, frameGrayscale.Height / 3));
 
                 foreach (System.Drawing.Rectangle roi in hands)
                 {
                     Gray colorcillo = new Gray(double.MaxValue);
-                    frameDepth.Draw(roi, colorcillo, 3);
+                    frameGrayscale.Draw(roi, colorcillo, 3);
                 }
             }
 
-            return frameDepth;
+            return frame = frameGrayscale.Convert<Bgra, Byte>(); 
         }//finaliza detection()
 
 
         //:::::::::::::Method to convert a byte[] to a writeablebitmap::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-        private WriteableBitmap imagetoWriteablebitmap(Image<Gray, Byte> frameHand)
+        private WriteableBitmap imagetoWriteablebitmap(Image<Bgra, Byte> frameHand)
         {
-            byte[] imagenPixels = new byte[DepthStream.FrameWidth * DepthStream.FrameHeight];
+            byte[] imagenPixels = new byte[ColorStream.FramePixelDataLength];
 
-            this.ImagenWriteablebitmap = new WriteableBitmap(DepthStream.FrameWidth, DepthStream.FrameHeight, 96, 96, PixelFormats.Gray8, null);
-            this.WriteablebitmapRect = new Int32Rect(0, 0, DepthStream.FrameWidth, DepthStream.FrameHeight);
-            this.WriteablebitmapStride = DepthStream.FrameWidth;
+            this.ImagenWriteablebitmap = new WriteableBitmap(ColorStream.FrameWidth, ColorStream.FrameHeight, 96, 96, PixelFormats.Bgr32, null);
+            this.WriteablebitmapRect = new Int32Rect(0, 0, ColorStream.FrameWidth, ColorStream.FrameHeight);
+            this.WriteablebitmapStride = ColorStream.FrameWidth* ColorStream.FrameBytesPerPixel;
 
             imagenPixels = frameHand.Bytes;
             ImagenWriteablebitmap.WritePixels(WriteablebitmapRect, imagenPixels, WriteablebitmapStride, 0);
@@ -223,9 +204,9 @@ namespace HandRGBDetection
 
 
         //::::::::::::Method to remove the noise, using median filters::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-        private Image<Gray, Byte> removeNoise(Image<Gray, Byte> imagenKinet, int sizeWindow)
+        private Image<Bgra, Byte> removeNoise(Image<Bgra, Byte> imagenKinet, int sizeWindow)
         {
-            Image<Gray, Byte> imagenSinRuido;
+            Image<Bgra, Byte> imagenSinRuido;
 
             imagenSinRuido = imagenKinet.SmoothMedian(sizeWindow);
 
@@ -235,7 +216,7 @@ namespace HandRGBDetection
 
         //:::::::::::::Method to saves the images with tha detection ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-        private void guardaimagen(Image<Gray, Byte> imagen, string path, int i, string ruido)
+        private void guardaimagen(Image<Bgra, Byte> imagen, string path, int i, string ruido)
         {
             //path ejemplo "C:\imagenClassifiersWitoutNoise\Ilumination\twoHands\Noise\";
             imagen.Save(path + ruido + @"\" + i.ToString() + ".png");
@@ -244,7 +225,7 @@ namespace HandRGBDetection
 
         private void botonGrabar_Click(object sender, RoutedEventArgs e)
         {
-            path = @"C:\checkDetectionRGB\" + nameClassifier + @"\" + tipoIluminacion + @"\" + numeroManos + @"\";
+            path = @"C:\checkDetectionRGB\" + background + @"\" + nameClassifier + @"\" + tipoIluminacion + @"\" + numeroManos + @"\";
             grabar = true;
             index = 0; 
         }//end
@@ -281,7 +262,28 @@ namespace HandRGBDetection
         {
             numeroManos = "2";
             unaMano.IsEnabled = false; 
+        }        
+        
+        
+        private void simple_Checked(object sender, RoutedEventArgs e)
+        {
+            background = "Simple";
+
+            complejo.IsEnabled = false; 
+            noiluminacion.IsEnabled = true;
+            iluminacion.IsEnabled = true;  
         }
+
+
+        private void complejo_Checked(object sender, RoutedEventArgs e)
+        {
+            background = "Complex";
+
+            simple.IsEnabled = false; 
+            noiluminacion.IsEnabled = true;
+            iluminacion.IsEnabled = true; 
+        }
+
         //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
 
 
@@ -307,6 +309,8 @@ namespace HandRGBDetection
         {
             Kinect.Stop();
         }
+
+
         //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
     
 
